f5b2c8c6ca96cb1f565d8f5790a50a8f0e7ebe43
冰彦糖 <44092516+LeagueJhin@users.noreply.github.com>
2019-11-22 13:41:23 +0800

    update 修正

--- 前端/js/index.js
+++ 前端/js/index.js
@@ -1,7 +1,7 @@
 /*
  * @Author: 冰彦糖
  * @Date: 2019-11-22 09:15:49
- * @LastEditTime: 2019-11-22 11:55:30
+ * @LastEditTime: 2019-11-22 13:41:04
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \前端\js\index.js
@@ -10,5 +10,5 @@
     var img_arr = new this.Array;
     // var img_tag = document.getElementsByClassName("main");
     var rand = Math.floor(Math.random() * 10);
-    $(".main").css("backgroundImage","url(/image/index/"+ rand + ".jpg)");
+    $(".main").css("backgroundImage","url(image/index/"+ rand + ".jpg)");
 })()
