fe7ef652d6794fd90520fe6552d6508a50a12754
冰彦糖 <44092516+LeagueJhin@users.noreply.github.com>
2019-11-25 00:46:59 +0800

    update 静态index完成
    静态完成index 待补充动效和其他页面，

--- 前端/js/index.js
+++ 前端/js/index.js
@@ -1,14 +1,46 @@
 /*
  * @Author: 冰彦糖
  * @Date: 2019-11-22 09:15:49
- * @LastEditTime: 2019-11-22 13:41:04
- * @LastEditors: Please set LastEditors
+ * @LastEditTime: 2019-11-25 00:38:25
+ * @LastEditors: 冰彦糖
  * @Description: In User Settings Edit
  * @FilePath: \前端\js\index.js
  */
+//const
 (window.onload = function() {
+    yiyan();
+    this.bgc();
+})()
+$(".main")[0].style.height = document.documentElement.clientHeight + "px";
+window.onresize = function() {//窗口变化
+    updatebgc();
+}
+function bgc() {
     var img_arr = new this.Array;
-    // var img_tag = document.getElementsByClassName("main");
     var rand = Math.floor(Math.random() * 10);
     $(".main").css("backgroundImage","url(image/index/"+ rand + ".jpg)");
-})()
+}
+function updatebgc() {
+    if (document.documentElement.clientHeight < screen.height * 0.9) {
+        $(".main")[0].style.height = screen.height/10 + "rem";
+    } else {
+        $(".main")[0].style.height = document.documentElement.clientHeight/10 + "rem";
+    }
+    // if (document.documentElement.clientWidth < screen.width * 0.9) {
+    //     $(".main")[0].style.width = screen.width/10 + "rem";
+    // } else {
+    //     $(".main")[0].style.width = document.documentElement.clientWidth/10 + "rem";
+    // }
+}
+function yiyan() {
+    fetch('https://v1.hitokoto.cn')
+    .then(function (res){
+        return res.json();
+    })
+    .then(function (data) {
+        $("#yiyan")[0].innerText = data.hitokoto; 
+    })
+    .catch(function (err) {
+        console.error(err);
+    })
+}
